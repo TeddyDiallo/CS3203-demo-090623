0df8473eda96c0fb50ae31c209688f2889ff3cc9
TeddyDiallo <teddy.f.diallo-1@ou.edu>
2023-09-10 17:32:40 -0500

    Add two more tests

--- test.py
+++ test.py
@@ -9,3 +9,14 @@ if app.is_palindrome('dog'):
     print("Test failed")
 else:
     print('Test passed')
+
+if app.is_palindrome('robber'):
+    print('Test failed')
+else:
+    print('Test passed')
+
+if app.is_palindrome('i'):
+    print('Test passed')
+else:
+    print('Test failed')
+
